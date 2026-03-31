c44b1b06e9e8cf8c205f4e35526db58b5a0367ad
Mrowr Purr <mrowr.purr@gmail.com>
2026-03-31 00:28:57 -0700

    🪟 Update espeak-ng hash (Windows MSVC fix)

--- packages/e/espeak-ng/xmake.lua
+++ packages/e/espeak-ng/xmake.lua
@@ -4,7 +4,7 @@ package("espeak-ng")
     set_license("GPL-3.0")
     add_urls("https://github.com/BuildWithCollab/collab-espeak-ng-fork/archive/refs/tags/$(version).tar.gz")
 -- [[ GENERATED:versions ]]
-    add_versions("2026.03.30-test1", "32c07ab8d7070ec3836628718ab292356bb83d91a14d47ab2e9259d2ddf4d635")
+    add_versions("2026.03.30-test1", "80b9a44499ab2d286003faa833a809497cfda7bfa413a547f380505a21c8114f")
 -- [[ /GENERATED:versions ]]
 -- [[ GENERATED:deps ]]
 -- [[ /GENERATED:deps ]]
