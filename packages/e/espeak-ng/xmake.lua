0a6fdac64dc69e294d959622e45fff213a37b939
Mrowr Purr <mrowr.purr@gmail.com>
2026-03-30 23:58:15 -0700

    🐛 Update espeak-ng hash (suppress warnings)

--- packages/e/espeak-ng/xmake.lua
+++ packages/e/espeak-ng/xmake.lua
@@ -4,7 +4,7 @@ package("espeak-ng")
     set_license("GPL-3.0")
     add_urls("https://github.com/BuildWithCollab/collab-espeak-ng-fork/archive/refs/tags/$(version).tar.gz")
 -- [[ GENERATED:versions ]]
-    add_versions("2026.03.30-test1", "497f22336c0c13d334e70023b6dfb7b448252fff3c1f8f685b959d21c311996d")
+    add_versions("2026.03.30-test1", "10cebde89b32d6ae89b6818c805c11bc38b8ec7c4da9ec8ef994569649cec093")
 -- [[ /GENERATED:versions ]]
 -- [[ GENERATED:deps ]]
 -- [[ /GENERATED:deps ]]
