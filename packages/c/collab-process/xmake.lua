80986a87fb6a27547c2befd2261f94a02ca3c8ca
Mrowr Purr <mrowr.purr@gmail.com>
2026-03-31 19:08:51 -0700

    collab-process v1.0.0

--- packages/c/collab-process/xmake.lua
+++ packages/c/collab-process/xmake.lua
@@ -7,9 +7,10 @@ package("collab-process")
     add_versions("v0.1.0", "6dcf3892cfa577fe8c0a8b1cc6cce18c9826806de849bc16c294ff120c0623bc")
     add_versions("v0.2.0", "86d8acea2984f165575d2c9ec9c9fc62be7002da02a5f0e189f292e3d14300cc")
     add_versions("v0.3.0", "f40d07a5e0c382e689cb87849745a4ad6fa5c015e270aca2ae5331a80ff4e91f")
+    add_versions("v1.0.0", "c94dcff16e4fd445616e19292b2b2e052bf5e2d3a46e55ecb9220dbda5159ee8")
 -- [[ /GENERATED:versions ]]
 -- [[ GENERATED:deps ]]
-    add_deps("collab-core 0.3.x")
+    add_deps("collab-core")
 -- [[ /GENERATED:deps ]]
     on_install(function (package)
 -- [[ GENERATED:install ]]
