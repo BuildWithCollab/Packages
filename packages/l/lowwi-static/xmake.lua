8d70ac7724557f31e4bb8a7d56a93b08d2dab40b
Mrowr Purr <mrowr.purr@gmail.com>
2026-04-03 03:41:25 -0700

    Add lowwi-static and collab-process v1.1.1

--- packages/l/lowwi-static/xmake.lua
+++ packages/l/lowwi-static/xmake.lua
@@ -4,6 +4,7 @@ package("lowwi-static")
     set_license("Apache-2.0")
     add_urls("https://github.com/BuildWithCollab/collab-lowwi-fork/archive/refs/tags/$(version).tar.gz")
 -- [[ GENERATED:versions ]]
+    add_versions("v3.0", "bce8d5476267904c4dafa7a90586e0d70e2f6120049b552ad407451dc1053ebd")
 -- [[ /GENERATED:versions ]]
 -- [[ GENERATED:deps ]]
     add_deps("onnxruntime-static")
