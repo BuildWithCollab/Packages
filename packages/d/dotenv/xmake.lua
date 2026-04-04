b7eba37a7e9f4025833e8a1a0619c6d46f8131b0
Mrowr Purr <mrowr.purr@gmail.com>
2026-04-04 07:24:26 -0700

    Add dotenv

---
+++ packages/d/dotenv/xmake.lua
@@ -0,0 +1,17 @@
+package("dotenv")
+    set_homepage("https://github.com/BuildWithCollab/dotenv")
+    set_description(".env library and CLI 🔧")
+    set_license("0BSD")
+    add_urls("https://github.com/BuildWithCollab/dotenv/archive/refs/tags/$(version).tar.gz")
+-- [[ GENERATED:versions ]]
+    add_versions("v1.0.2", "760a09c4b114436153e93677edeffe75d06af71f9c2e1f08469c30d3718fba8e")
+-- [[ /GENERATED:versions ]]
+-- [[ GENERATED:deps ]]
+    add_deps("collab-core")
+    add_deps("collab-process")
+-- [[ /GENERATED:deps ]]
+    on_install(function (package)
+-- [[ GENERATED:install ]]
+        import("package.tools.xmake").install(package)
+-- [[ /GENERATED:install ]]
+    end)
