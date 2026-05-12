232f1816951d0458ba6fe5f46960b32c6e6bbd02
Mrowr Purr <mrowr.purr@gmail.com>
2026-05-12 16:25:00 -0700

    collab-core v0.8.2

--- packages/c/collab-core/xmake.lua
+++ packages/c/collab-core/xmake.lua
@@ -12,6 +12,7 @@ package("collab-core")
     add_versions("v0.7.0", "08f63649f4d77a5968dfe6eea6523ff4008190248e07f04583ccb8fff63d3d74")
     add_versions("v0.8.0", "2ca3f680d3d067093dca83f1d2f5c538e0022fa65ac9473e632780890c2bdd5e")
     add_versions("v0.8.1", "7fb50bc854e9be6a28d32191c1fa90af22dd74d66d537d45127d1e623a8ab7d8")
+    add_versions("v0.8.2", "4b65ec7ff740be708b385c7100def49c7c8c7dde9a59b4403cf1b0cd960df897")
 -- [[ /GENERATED:versions ]]
 -- [[ GENERATED:deps ]]
     add_deps("fmt")
